2bc046c0e4bae9a3327342560ccca9728c821e56
Syed Laeeq Ahmed <laeeqe@icloud.com>
2025-07-12 13:34:02 -0700

    home page implement

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,50 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-   <div>
-    <h1 className='text-2xl underline'>Welcome to the LLM</h1>
-    <Button>Let's get started</Button>
-   </div>
+   <main>
+    <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Networks"
+          subject = "science"
+          duration={45}
+          color = "#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Linguo the Language Lover"
+          topic="Natural Language Processing"
+          subject = "language"
+          duration={30}
+          color = "#6ee7b7"/>
+          <CompanionCard 
+          id="789"
+          name="Data the Data Detective"
+          topic="Data Analysis"
+          subject = "data"
+          duration={60}
+          color = "#f87171"/>
+  
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList
+        title = "Recently Completed Sessions"
+        companions={recentSessions}
+        className="w-2/3 max-lg:w-full"
+        />
+        <CTA/>
+
+      </section>
+   </main>
   )
 }
 
